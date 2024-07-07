cdd98ed451bb5b3729d80390d0044a9cd941d138
DESKTOP-UEGMPNS\Кирило <skiril502@gmail.com>
2024-07-07 12:39:00 +0300

    I add new controller with calculator function

---
+++ WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GreetGet(int num1, int num2, char key)
+        {
+            string result = "";
+
+            switch (key)
+            {
+                case '+':
+                    result = $"Sum = {num1 + num2}";
+                    break;
+                case '-':
+                    result = $"Subtract = {num1 - num2}";
+                    break;
+                case '*':
+                    result = $"Multiply = {num1 * num2}";
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        return BadRequest("Division by zero is not allowed.");
+                    }
+                    result = $"Divide = {num1 / num2}";
+                    break;
+            }
+            return Ok(result);
+        }
+    }
+}
